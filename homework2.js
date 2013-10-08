a55eae66b51b7ce38fbca2a61fd803d97aeb5e7b
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-10-07 23:50:07 -0400

    Homework 2 corrections

--- homework2.js
+++ homework2.js
@@ -14,16 +14,27 @@
 // The total cost of a 10 dollar book plus 5% sales tax
 // Whether or not the light switch is on
 // Whether or not Superman can fly
-var  email = ("fionanyamira@gmail.com"),
-     major = ("Web Design");
-     favoriteEditor = ("Sublime for now"),
-     zipCode = ("20903"),
-     bookCost = ("10 + 10/5"),
-     pi = ("3.14159"),
-     lightSwitch = false,
-     SupermanFlies = true;
 
-     console.log (email, major, favoriteEditor, zipCode, bookCost, pi, lightSwitch, SupermanFlies);
+var  email = "fionanyamira@gmail.com",
+     major = "Web Design", //JG: Careful here. The semi-colon here meant that the rest of the variables weren't declared with a "var". While this works it can introduce problems when done inside functions as the variables without a "var" are considered global.
+     favoriteEditor = "Sublime for now",
+     zipCode = "20903",
+     bookCost = 10 * 1.05, //JG: This formula doesn't work and it should be numeric, not a string. See corrected formula.
+     pi = 3.14159, //JG: Should be numeric
+     lightSwitchIsOn = false,
+     supermanCanFly = true; //JG: Don't start a variable name with a capital letter
+
+//JG: Improved readability
+console.log(
+  email, 
+  major, 
+  favoriteEditor, 
+  zipCode, 
+  bookCost, 
+  pi, 
+  lightSwitchIsOn, 
+  supermanCanFly
+);
 
 
 
@@ -32,14 +43,15 @@ var  email = ("fionanyamira@gmail.com"),
 // console.log(typeof myMajor);
 // and so on…
 // Make sure you have at least one string, at least one number, and at least one Boolean.
-   console.log(typeof email);     
-   console.log(typeof major);
-   console.log(typeof favoriteEditor);
-   console.log(typeof zipCode);
-   console.log(typeof bookCost);
-   console.log(typeof pi);
-   console.log(typeof lightSwitch);
-   console.log(typeof SupermanFlies);
+
+console.log(typeof email);     
+console.log(typeof major);
+console.log(typeof favoriteEditor);
+console.log(typeof zipCode);
+console.log(typeof bookCost);
+console.log(typeof pi);
+console.log(typeof lightSwitchIsOn);
+console.log(typeof supermanCanFly);
 
 // Problem 4) Create string variables to store the following string values.
 // Send all variables to the console.
@@ -47,17 +59,22 @@ var  email = ("fionanyamira@gmail.com"),
 // <a href="#">Some link</a>
 // JavaScript is "easy" if you are good at it
 
-var badDog = ("The dog's biting my shoes");
-    link = ("<a href = '#'>www.ms54.com</a>");
-    skills = ("JavaScript is "easy"if you are good at it.");
+var badDog = "The dog's biting my shoes", //JG: Again, be careful with the semi-colons
+    link = "<a href = '#'>www.ms54.com</a>",
+    skills = "JavaScript is \"easy\"if you are good at it."; //JG: This line of code breaks because the string isn't properly defined.
 
-    console.log (badDog,link,skills);
+console.log(badDog,link,skills);
 
 
 // Problem 5) Create an array of topics you would like to learn this semester.
 // Send this array to the console.
-var myArray = ['better usnderstanding JavaScript', ' variables', ' understanding statements applied in the function'];
-    console.log (myArray);
+var myArray = [
+  'better usnderstanding JavaScript', 
+  'variables', 
+  'understanding statements applied in the function'
+];
+
+console.log (myArray);
 
 
 // Problem 6) Send each item of the last array to the console.
@@ -65,15 +82,17 @@ var myArray = ['better usnderstanding JavaScript', ' variables', ' understanding
 // console.log(myArray[0])
 // console.log(myArray[1])
 // and so on…
-    console.log(myArray[0]);
-    console.log(myArray[1]);
-    console.log(myArray[2]);
+
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
 
      
 // Problem 7) Use the yourArrayVariable.length property to determine the size of your array.
 // Send the result to the console.
-    myArray.length;
-    console.log(myArray.length);
+
+myArray.length;
+console.log(myArray.length);
 
 
 // Problem 8) Create a function named "clickMe".
@@ -82,9 +101,8 @@ var myArray = ['better usnderstanding JavaScript', ' variables', ' understanding
 // hint: Use document.getElementById to get a reference to the button in
 // a similar way to how we did it in class for the currency conversion
 // calculator.
-  document.getElementById("clickMe").onClick = function()
-  {
-  	"Leave me ALONE!";
-  };
 
+//JG: This wouldn't work before because "onclick" has to be all lowercase. Did you test it to make sure? If so, what happened?
+document.getElementById("clickMe").onclick = function() { 
   console.log("Leave me ALONE")
+};
