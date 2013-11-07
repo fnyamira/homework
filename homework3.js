558d82650970b14641df32d2dd183162da638957
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-11-06 22:57:23 -0500

    Homework 3 fixes

--- homework3.js
+++ homework3.js
@@ -1,29 +1,27 @@
 //Fiona Nyamira
 //problem 1
 
-console.log(5+5);
-console.log(5-5);
-console.log(5*5);
-console.log(5/5);
+console.log(5 + 5);
+console.log(5 - 5);
+console.log(5 * 5);
+console.log(5 / 5);
 
 //problem 2
 
-function consequence(brokeVase) {
-	if (brokeVase === 'dad') {
+function consequence(cookieThief) {
+	if (cookieThief === 'dad') {
 		return "He's going to be in major trouble!";
 	}
-}
 
-if (brokeVase === 'mum'){
-	return "No big deal.";
-}
+	if (cookieThief === 'mum') {
+		return "No big deal.";
+	}
 
-if (brokeVase === "Me") {
-	return " No more cookies for you";
-}
+	if (cookieThief === "Me") {
+		return " No more cookies for you";
+	}
 
-return "Johnny did it";
-
+	return "Johnny did it";
 }
 
 console.log(consequence('Mum'));
@@ -35,27 +33,29 @@ function place() {
 	var day = 'Thursday';
 	var time = '6:30PM';
 	var room = 'HU 317';
-}
 
-if (day ==='Thursday' && time === '6:30PM' && room === 'HU317'){
-	console.log("You are in CA276");
+	if (day === 'Thursday' && time === '6:30PM' && room === 'HU317') {
+		console.log("You are in CA276");
+	}
 }
-
 //problem 4
 
-var bartSimpson ='';
+var bartSimpson = '';
 var i = 0;
 
-while (i < 12) {
-	i+='I will be nice to my sister and Homer "Riggggght he! he! he1.';
+while (i < 10) {
+	bartSimpson += 'I will be nice to my sister and Homer "Riggggght he! he! he1.';
+	i += 1;
 }
+
+console.log(bartSimpson);
 
 //Problem 5
 
 bartSimpson = '';
 
-for (i = 0; i<12; i+=1) {
-	bartSimpson+='I will be nice to my sister and Homer "Riggggght he! he! he1.';
+for (i = 0; i < 10; i += 1) {
+	bartSimpson += 'I will be nice to my sister and Homer "Riggggght he! he! he1.';
 }
 
 console.log(bartSimpson);
